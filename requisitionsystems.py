78de6b603c0458a00f519fb8ec216ee7f32bc5c7
RayyanAlAnsari <msuhail6497@gmail.com>
2025-09-19 12:37:38 +1200

    requisitionsystems.py

--- requisitionsystems.py
+++ requisitionsystems.py
@@ -2,7 +2,7 @@
 This code was done using Object-oriented programming and all of the codes from previous task were combined with few changes to meet the requirements, in the output, statistics is given as output and manager can also decide wheter to Approve/Not Approve/Pending 
 '''
 class RequisitionSystem():
-
+#this is a class and below are empty variables with no values, which later will help us complete the statistics
     counter = 0
     staffs = []
     total_requisitions = 0
@@ -11,6 +11,7 @@ class RequisitionSystem():
     total_notapproved = 0
 
     def __init__(self):
+#this is a constructor which helps us in assigning values (Open/Closed is applied since we can make extension but no modification)
         self.date = None
         self.staff_id = None
         self.staff_name = None
@@ -21,6 +22,7 @@ class RequisitionSystem():
         RequisitionSystem.staffs.append(self)
     
     def staff_info(self):
+#staff functions allows user to input basic staff information and assign a unique ID (YAGNI can be applied in line 40 and 45, since that line is only a cosmetic feature and its not important as of now)
         print("Printing Staff Information: ")
         RequisitionSystem.counter += 1
         self.date = input("Date: ")
@@ -42,7 +44,8 @@ class RequisitionSystem():
         print(f"Requisition ID: {self.unique_id}")
         print("---------------------------------------------")
 
-    def requisitions_details(self):        
+    def requisitions_details(self): 
+#this function allows user to enter number of items and their price (clean code > clever code is used here because its easy to read and maintain)
         self.cost = 0
         num_items = int(input("How many items are you buying: "))
         for i in range(num_items):
@@ -53,6 +56,7 @@ class RequisitionSystem():
         print(f"Total cost is ${self.cost}")
 
     def requisition_approval(self):
+#this function checks if the total is greater than, equal or less than 500, and based on that approval is granted or denied (KISS is used here because verything is simple and straightforward)
         RequisitionSystem.total_requisitions += 1
         if self.cost >= 500:
             self.status = "Pending"
@@ -66,6 +70,7 @@ class RequisitionSystem():
             RequisitionSystem.total_approved += 1
 
     def respond_requisition(self):
+#this function allows manager to respond to all the requistions and decide whether to approve/deny a requisiton (Refactor can be used in the future for this function to improve it)   
         for staff in RequisitionSystem.staffs:
             if staff.status == "Pending":
                 decision = input("Enter decision-Approved/Not Approved/Pending: ").lower()
@@ -83,7 +88,8 @@ class RequisitionSystem():
                     staff.status = "Pending"
                     staff.approval_num = "Not Assigned"
 
-    def display_requisitions(self):    
+    def display_requisitions(self):
+#this function displays all the information of the staff (KISS is used here because verything is simple and straightforward)
         for staff in RequisitionSystem.staffs:
             print("\nPrinting Requisitions: ")
             print(f"Date: {staff.date}")
@@ -95,6 +101,7 @@ class RequisitionSystem():
             print(f"Approval Reference number: {staff.approval_num}")
 
     def requisition_statistic(self):
+#this function displays all the statistics (KISS is used here because verything is simple and straightforward)
         print("\nStatistics:")
         print("Displaying the Requisition Statistics")
         print(f"The total number of requisitions submitted: {RequisitionSystem.total_requisitions}")
@@ -102,6 +109,7 @@ class RequisitionSystem():
         print(f"The total number of pending requisitions: {RequisitionSystem.total_pending}")
         print(f"The total number of not approved requisitions: {RequisitionSystem.total_notapproved}")
 
+#these are objects with instaces 
 r1 = RequisitionSystem()
 r1.staff_info()
 r1.requisitions_details()
@@ -129,4 +137,5 @@ r5.requisition_approval()
 
 r5.respond_requisition()
 r5.display_requisitions()
+
 r5.requisition_statistic()
